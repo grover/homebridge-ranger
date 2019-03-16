c3af489fb5478f21a92d8605d26cc507298f9b7b
Michael Froehlich <michael.ruck@michaelruck.de>
2019-03-16 01:18:09 +0100

    Enable pairing with fibaro flood sensor

--- src/hap/decoders/GATTValidRangeDecoder.js
+++ src/hap/decoders/GATTValidRangeDecoder.js
@@ -2,16 +2,21 @@
 const FormatDecoders = require('./../formats/FormatDecoders');
 
 function decode(metadata, buffer) {
-  const offset = FormatDecoders.getLengthOfFormat(metadata.format);
+  try {
+    const offset = FormatDecoders.getLengthOfFormat(metadata.format);
 
-  const minValue = FormatDecoders.decode(metadata.format, buffer, 0, buffer.length);
-  if (Number.isFinite(minValue) && !Number.isNaN(minValue)) {
-    metadata.minValue = minValue;
+    const minValue = FormatDecoders.decode(metadata.format, buffer, 0, buffer.length);
+    if (Number.isFinite(minValue) && !Number.isNaN(minValue)) {
+      metadata.minValue = minValue;
+    }
+
+    const maxValue = FormatDecoders.decode(metadata.format, buffer, offset, buffer.length);
+    if (Number.isFinite(maxValue) && !Number.isNaN(maxValue)) {
+      metadata.maxValue = maxValue;
+    }
   }
-
-  const maxValue = FormatDecoders.decode(metadata.format, buffer, offset, buffer.length);
-  if (Number.isFinite(maxValue) && !Number.isNaN(maxValue)) {
-    metadata.maxValue = maxValue;
+  catch (e) {
+    // Invalid format, ignore the range descriptor
   }
 }
 
